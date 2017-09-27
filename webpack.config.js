8382a9c0c7c35fb047d0a8410f475a5720ed1762
MikeDiaz1 <diaz-@live.ca>
2017-09-27 08:36:27 -0400

    Added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+
+module.exports = {
+
+    entry: path.resolve(__dirname, 'src') + '/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'src'),
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            },
+            {
+                test: /\.scss$/,
+                loader: 'style-loader!css-loader!sass-loader'
+            }
+        ]
+    }
+};
