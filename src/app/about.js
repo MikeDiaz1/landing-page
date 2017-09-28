2d45ed99d0956dd9d7458bc6729f2e4ca326c5e7
MikeDiaz1 <diaz-@live.ca>
2017-09-27 22:31:03 -0400

    added about page skeleton

---
+++ src/app/about.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+class About extends React.Component {
+    render(){
+        return(
+            <div>This is the about</div>
+        );
+    }
+}
+
+module.exports = About;
