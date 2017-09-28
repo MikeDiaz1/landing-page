438a8f22a236de93dba1d63fba110ddf8a363704
MikeDiaz1 <diaz-@live.ca>
2017-09-27 23:26:54 -0400

    basic footer structure

--- src/app/footer.js
+++ src/app/footer.js
@@ -1,9 +1,37 @@
 var React = require('react');
+require('./css/footer.scss');
 
 class Footer extends React.Component {
     render(){
         return(
-            <footer>This is the footer</footer>
+            <footer>
+                <FooterHeaders />
+                <div id='footer-bottom'>
+                    bottom
+                </div>
+            </footer>
+        );
+    }
+}
+
+class FooterHeaders extends React.Component{
+
+    constructor(props){
+        super(props)
+        this.state = {
+            headers: ['Catagories', 'Help', 'My Account', 'About', 'Social']
+        };
+    }
+
+    render (){
+        var headers = this.state.headers;
+        headers = headers.map( function(item, index) {
+            return (
+                <div>{item}</div>
+            );
+        })
+        return(
+            <div id='footer-top'>{headers}</div>
         );
     }
 }
