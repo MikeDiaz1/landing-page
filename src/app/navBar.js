b74d5d4280b910956b9301cc203ccf72bb085d91
MikeDiaz1 <diaz-@live.ca>
2017-09-27 22:31:12 -0400

    added nav bar skeleton

---
+++ src/app/navBar.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+class Nav extends React.Component {
+    render(){
+        return(
+            <nav>This is the nav bar</nav>
+        );
+    }
+}
+
+module.exports = Nav;
