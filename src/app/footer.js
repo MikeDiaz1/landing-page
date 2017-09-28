48cbe7bf5c64d732fb4141f229565f56927d5506
MikeDiaz1 <diaz-@live.ca>
2017-09-28 01:30:01 -0400

    flushed out footer, created body sections

--- src/app/footer.js
+++ src/app/footer.js
@@ -5,35 +5,64 @@ class Footer extends React.Component {
     render(){
         return(
             <footer>
-                <FooterHeaders />
-                <div id='footer-bottom'>
-                    bottom
-                </div>
+                <FooterTop />
+                <FooterBottom />
             </footer>
         );
     }
 }
 
-class FooterHeaders extends React.Component{
+class FooterTop extends React.Component {
 
     constructor(props){
         super(props)
         this.state = {
-            headers: ['Catagories', 'Help', 'My Account', 'About', 'Social']
+            headers: ['Catagories', '', 'Help', 'My Account', 'About', 'Social'],
+            listItems: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6']
         };
     }
 
     render (){
         var headers = this.state.headers;
+        var listItems = this.state.listItems;
+
+        listItems = listItems.map( function(item, index) {
+            return(
+                <div><a href='#'>{item}</a></div>
+            )
+        })
+
+
         headers = headers.map( function(item, index) {
             return (
-                <div>{item}</div>
+                <div>
+                    <strong>{item}</strong>
+                    {listItems}
+                </div>
             );
         })
+
         return(
             <div id='footer-top'>{headers}</div>
         );
     }
+}
+
+class FooterBottom extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render (){
+        return(
+            <div id='footer-bottom'>
+                <hr></hr>
+                <span>&copy; Michael Diaz-Stewart</span>
+            </div>
+        );
+    }
+
 }
 
 module.exports = Footer;
--- src/app/index.js
+++ src/app/index.js
@@ -27,15 +27,11 @@ class Main extends React.Component {
             <div>
                 <Nav />
                 <div id="content">
-                    <div>1</div>
-                    <div>2</div>
-                    <div>3</div>
-                    <div>4</div>
-                    <div>5</div>
-                    <div>6</div>
-                    <div>7</div>
-                    <div>8</div>
-                    <div>9</div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
                 </div>
                 <Footer />
             </div>
