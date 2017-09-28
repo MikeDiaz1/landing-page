4f626864e1e340a3080ce35f418bd1b80c731acc
MikeDiaz1 <diaz-@live.ca>
2017-09-27 22:31:39 -0400

    added component imports

--- src/app/index.js
+++ src/app/index.js
@@ -1,15 +1,19 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 import {BrowserRouter as Router, Route} from 'react-router-dom';
-
 require('./css/index.scss');
+
+//Component imports
+import Nav from './navBar';
+import Footer from './footer';
+import About from './about';
 
 class App extends React.Component {
     render (){
         return(
             <Router>
                 <div>
-                    <Route exact path='/' component={Grid}/>
+                    <Route exact path='/' component={Main}/>
                     <Route exact path='/about' component={About}/>
                 </div>
             </Router>
@@ -17,28 +21,13 @@ class App extends React.Component {
     }
 }
 
-class About extends React.Component {
-    render(){
-        return(
-            <p>This is an about page</p>
-        );
-    }
-
-}
-
-class Grid extends React.Component {
+class Main extends React.Component {
     render (){
         return(
-            <div id='grid'>
-                <div>1</div>
-                <div>2</div>
-                <div>3</div>
-                <div>4</div>
-                <div>5</div>
-                <div>6</div>
-                <div>7</div>
-                <div>8</div>
-                <div>9</div>
+            <div>
+                <Nav />
+                <h1>This is the body</h1>
+                <Footer />
             </div>
         );
     }
