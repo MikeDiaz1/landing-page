e8ec36c4e2554bc169dfc5119653c004aec7761b
MikeDiaz1 <diaz-@live.ca>
2017-10-05 19:44:20 -0400

    Added footer

--- src/app/screens/Home.js
+++ src/app/screens/Home.js
@@ -1,36 +1,45 @@
 import React, { Component } from 'react';
 
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer'
 
 export default class Home extends Component {
     render() {
         return (
             <div>
-            <section className="hero is-primary is-fullheight">
-                <Navbar />
-                <div className="hero-body">
-                    <div className="container">
-                        <h1 className="title">
-                            Hero title
+                <section className="hero is-primary is-fullheight">
+                    <Navbar />
+                    <div className="hero-body">
+                        <div className="container">
+                            <h1 className="title">
+                                Hero title
                             </h1>
-                        <h2 className="subtitle">
-                            Hero subtitle
+                            <h2 className="subtitle">
+                                Hero subtitle
                             </h2>
+                        </div>
                     </div>
-                </div>
-            </section>
+                </section>
 
-            <section className="hero is-white is-fullheight">
-            </section>
-            
-            <section className="hero is-dark is-fullheight">
-            </section>
-            
-            <section className="hero is-white is-fullheight">
-            </section>
-            
-            <section className="hero is-dark is-fullheight">
-            </section>
+                <section className="hero is-white is-fullheight">
+                </section>
+
+                <section className="hero is-dark is-fullheight">
+                </section>
+
+                <section className="hero is-white is-fullheight">
+                    <div className="hero-body">
+                        <div className="container">
+                            <h1 className="title">
+                                Hero title
+                            </h1>
+                            <h2 className="subtitle">
+                                Hero subtitle
+                            </h2>
+                        </div>
+                    </div>
+                    <Footer />
+                </section>
             </div>
         );
     }
