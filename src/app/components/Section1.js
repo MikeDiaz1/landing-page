ac32b05cb0df151a9a53875d8dc88bbda7e215b2
MikeDiaz1 <diaz-@live.ca>
2017-10-05 16:57:01 -0400

    Moved public source, linked favicon

--- src/app/components/Section1.js
+++ src/app/components/Section1.js
@@ -7,13 +7,18 @@ import Section from 'grommet/components/Section';
 import Image from 'grommet/components/Image'
 import Hero from 'grommet/components/Hero'
 import Header from 'grommet/components/Header'
+import Heading from 'grommet/components/Heading'
+import Footer from 'grommet/components/Footer'
+import Button from 'grommet/components/Button'
 import Box from 'grommet/components/Box'
 import Form from 'grommet/components/Form'
+import FormFields from 'grommet/components/FormFields'
+import TextInput from 'grommet/components/TextInput'
 
 export default class Section1 extends Component {
     render() {
         return (
-            <Section pad='none' justify='start' align='stretch' full='vertical' colorIndex='brand'>
+            <Section pad='none' justify='start' align='stretch' full='vertical' colorIndex='light-1'>
                 <Hero size='large' backgroundColorIndex='light-1' background={<Image src={hero} fit='cover' />}>
                 </Hero>
                 <Header float={true}>
@@ -21,8 +26,13 @@ export default class Section1 extends Component {
                         <Image src={uproposTitle} size='medium' />
                     </Box>
                 </Header>
-                <Box full={true}>
-
+                <Box align='center' justify='center' full={true}>
+                    <Form plain={true}>
+                        <FormFields>
+                        <Heading>Label</Heading>
+                        <TextInput />
+                        </FormFields>
+                    </Form>
                 </Box>
             </Section>
         )
