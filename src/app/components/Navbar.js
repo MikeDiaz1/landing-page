a858bd60444fd25f17d84d52431317bf2acf280f
MikeDiaz1 <diaz-@live.ca>
2017-10-05 20:53:30 -0400

    Section links

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -5,7 +5,7 @@ import logo from '../../../public/img/Logo-white.png';
 export default class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar is-dark">
+            <nav className="navbar">
                 <div className="navbar-brand">
                     <a className="navbar-item" href="#">
                         <img src={logo} alt="Upropos: The Future of Crowdfunding"></img>
@@ -13,10 +13,10 @@ export default class Navbar extends Component {
                 </div>
                 <div className="navbar-menu">
                     <div className="navbar-end">
-                        <a className="nav-item" href="#">Section 1</a>
-                        <a className="nav-item" href="#">Section 2</a>
-                        <a className="nav-item" href="#">Section 3</a>
-                        <a className="nav-item" href="#">Section 4</a>
+                        <a className="nav-item" href="#section-1">Section 1</a>
+                        <a className="nav-item" href="#section-2">Section 2</a>
+                        <a className="nav-item" href="#section-3">Section 3</a>
+                        <a className="nav-item" href="#section-4">Section 4</a>
                     </div>
                 </div>
             </nav>
