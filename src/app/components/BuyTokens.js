40320337d31c2aa736b8cea74101947aab30acd3
MikeDiaz1 <diaz-@live.ca>
2017-10-08 11:55:11 -0400

    Add buy section

--- src/app/components/BuyTokens.js
+++ src/app/components/BuyTokens.js
@@ -3,53 +3,25 @@ import { CONTRACT } from '../contract.js'
 import _ from 'lodash'
 
 
-export default class SendTokens extends Component {
+export default class BuyTokens extends Component {
     constructor() {
         super()
         this.state = {
-            address: '',
-            amount: ''
+            amount: '',
+            rate: 10
         }
     }
 
-    send() {
-        if (!web3.isAddress(this.state.address)) {
-            this.setState({
-                address: ''
-            })
-            alert('Invalid address')
-            return
-        }
-
-        var amt = parseFloat(this.state.amount)
-        if (!_.isNumber(amt) || amt <= 0) {
-            this.setState({
-                amount: ''
-            })
-            alert('Invalid amount')
-            return
-        }
-
-        CONTRACT.transfer(this.state.address, web3.toWei(this.state.amount, 'ether'), (err, res) => {
+    componentWillMount() {
+        CONTRACT.rate((err, res) => {
             if (!err) {
-                console.log(res)
                 this.setState({
-                    address: '',
-                    amount: ''
+                    rate: res.toNumber()
                 })
-                return
             }
-            console.log(err)
-            this.setState({
-                address: '',
-                amount: ''
-            })
-        })
-    }
-
-    addressHandler(e) {
-        this.setState({
-            address: e.target.value
+            else {
+                console.log(err)
+            }
         })
     }
 
@@ -59,48 +31,61 @@ export default class SendTokens extends Component {
         })
     }
 
+    buy() {
+        var amt = parseFloat(this.state.amount)
+        if (!_.isNumber(amt) || amt <= 0) {
+            this.setState({
+                amount: ''
+            })
+            alert('Invalid amount')
+            return
+        }
+
+        CONTRACT.buyProcoin({ value: web3.toWei(this.state.amount / this.state.rate, 'ether') }, (err, res) => {
+            if (!err) {
+                console.log(res);
+                this.setState({
+                    amount: ''
+                })
+                return
+            }
+            console.log(err);
+            this.setState({
+                amount: ''
+            })
+        })
+    }
+
     render() {
         return (
             <div className="box">
                 <div className="level">
                     <div className="level-item">
                         <h2 className="title">
-                            Send
+                            Buy
                         </h2>
                     </div>
                 </div>
                 <div className="container has-text-centered">
                     <div className="columns">
-                        <div className="column is-6">
-                            <div className="field">
-                                <div className="control is-expanded has-icons-right">
-                                    <input className="input" type="text" value={this.state.address}
-                                        placeholder="To Address" onChange={this.addressHandler.bind(this)}>
-                                    </input>
-                                    <span className="icon is-small is-right">
-                                        <i className="fa fa-user-circle-o"></i>
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="column is-3">
+                        <div className="column is-4 is-offset-one-quarter">
                             <div className="field has-addons">
-                                <div className="control">
+                                <div className="control is-expanded">
                                     <input className="input" type="text" value={this.state.amount}
                                         placeholder="Amount" onChange={this.amountHandler.bind(this)}>
                                     </input>
                                 </div>
                                 <div className="control">
                                     <a className="button is-static">
-                                        Tokens
+                                       Tokens = {this.state.amount / this.state.rate} Ether
                                     </a>
                                 </div>
                             </div>
                         </div>
                         <div className="column is-2">
-                            <a className="button is-info" onClick={this.send.bind(this)}>
+                            <a className="button is-info" onClick={this.buy.bind(this)}>
                                 <span className="icon is-small">
-                                    <i className="fa fa-paper-plane"></i>
+                                    <i className="fa fa-shopping-cart"></i>
                                 </span>
                                 <span>Confirm Transfer</span>
                             </a>
