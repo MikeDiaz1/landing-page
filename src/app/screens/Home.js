67f10f31ed77cd7ca45a466abdc78166c8d0fd52
MikeDiaz1 <diaz-@live.ca>
2017-10-08 12:54:26 -0400

    Aesthetic changes

--- src/app/screens/Home.js
+++ src/app/screens/Home.js
@@ -9,44 +9,51 @@ export default class Home extends Component {
             <div>
                 <section id="section-1" className="parallax hero is-primary is-fullheight">
                     <Navbar />
+                    <div className="hero-body">
+                        <div className="container has-text-centered">
+                            <h1 className="subtitle is-1">
+                                We Are Upropos
+                            </h1>
+                            <h2 className="title is-1">
+                                The Future of Crowdfunding
+                            </h2>
+                        </div>
+                    </div>
                 </section>
 
-                <section id="section-2" className="hero is-fullheight">
-                    <section className="hero is-info is-bold is-medium">
+                <section id="section-2" className="hero is-light is-fullheight">
+                    <section className="hero is-dark is-medium">
                         <div className="hero-body">
-                            <div className="container">
+                            <div className="container has-text-centered">
                                 <h1 className="title">
-                                    Medium title
+                                    Section 1
                             </h1>
                                 <h2 className="subtitle">
-                                    Medium subtitle
+                                    Subtitle 1
                             </h2>
                             </div>
                         </div>
                     </section>
-
-                    <section className="hero is-light is-large">
-                        <div className="hero-body">
-                            <div className="container">
-                                <h1 className="title">
-                                    Large title
+                    <div className="hero-body">
+                        <div className="container has-text-centered">
+                            <h1 className="title">
+                                Section 2
                             </h1>
-                                <h2 className="subtitle">
-                                    Large subtitle
+                            <h2 className="subtitle">
+                                Subtitle 2
                             </h2>
-                            </div>
                         </div>
-                    </section>
+                    </div>
                 </section>
 
                 <section id="section-3" className="hero is-white is-fullheight">
                     <div className="hero-body">
-                        <div className="container">
+                        <div className="container has-text-centered">
                             <h1 className="title">
-                                Hero title
+                                Section 3
                             </h1>
                             <h2 className="subtitle">
-                                Hero subtitle
+                                Subtitle 3
                             </h2>
                         </div>
                     </div>
