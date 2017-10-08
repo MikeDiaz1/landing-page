be68b5fd2e24614170ac65a0d79798197f575bec
MikeDiaz1 <diaz-@live.ca>
2017-10-08 11:56:22 -0400

    Add loading of buy section to main demo component

--- src/app/screens/Demo.js
+++ src/app/screens/Demo.js
@@ -5,6 +5,7 @@ import logo from '../../../public/img/Logo.png'
 import WalletInfo from '../components/WalletInfo'
 import WalletLookup from '../components/WalletLookup'
 import SendTokens from '../components/SendTokens'
+import BuyTokens from '../components/BuyTokens'
 import Footer from '../components/Footer'
 
 
@@ -12,14 +13,14 @@ function Active(props) {
     const tab = props.tab;
     switch (tab) {
         case 0:
-            return <WalletInfo />
-            break;
-        case 1:
             return <WalletLookup />
-            break;
-        case 2:
+            break
+        case 1:
             return <SendTokens />
-            break;
+            break
+        case 2:
+            return <BuyTokens />
+            break
     }
 }
 
@@ -29,7 +30,7 @@ export default class Demo extends Component {
         super(props)
         this.state = {
             active: 0,
-            classes: ["is-active", "", "", ""]
+            classes: ["is-active", "", ""]
         }
     }
 
@@ -41,17 +42,17 @@ export default class Demo extends Component {
         switch (tab) {
             case 0:
                 this.setState({
-                    classes: ["is-active", "", "", ""]
+                    classes: ["is-active", "", ""]
                 })
                 break
             case 1:
                 this.setState({
-                    classes: ["", "is-active", "", ""]
+                    classes: ["", "is-active", ""]
                 })
                 break
             case 2:
                 this.setState({
-                    classes: ["", "", "is-active", ""]
+                    classes: ["", "", "is-active"]
                 })
                 break
         }
@@ -61,7 +62,7 @@ export default class Demo extends Component {
         return (
             <div>
                 <div className="hero is-fullheight is-light">
-                    <div className="container is-widescreen is-fullheight">
+                    <div className="container">
                         <div className="hero is-medium">
                             <div className="hero-head">
                                 <a href="/">
@@ -71,7 +72,7 @@ export default class Demo extends Component {
                             <div className="hero-body has-text-centered">
                                 <div className="container">
                                     <h1 className="title">
-                                    <img src={logo} alt="Upropos: The Future of Crowdfunding"></img>
+                                        <img src={logo} alt="Upropos: The Future of Crowdfunding"></img>
                                     </h1>
                                     <h2 className="subtitle">
                                         The Future of Crowdfunding
@@ -79,35 +80,30 @@ export default class Demo extends Component {
                                 </div>
                             </div>
                         </div>
-                        <div className="box has-text-centered">
+                        <div className="box">
                             <div className="tabs is-centered is-boxed">
                                 <ul>
                                     <li className={this.state.classes[0]}>
                                         <a onClick={() => { this.tabHandler(0) }}>
-                                            <span className="icon is-small"><i className="fa fa-user"></i></span>
-                                            <span>My Wallet</span>
-                                        </a>
-                                    </li>
-                                    <li className={this.state.classes[1]}>
-                                        <a onClick={() => { this.tabHandler(1) }}>
                                             <span className="icon is-small"><i className="fa fa-search"></i></span>
                                             <span>Wallet Lookup</span>
                                         </a>
                                     </li>
-                                    <li className={this.state.classes[2]}>
-                                        <a onClick={() => { this.tabHandler(2) }}>
+                                    <li className={this.state.classes[1]}>
+                                        <a onClick={() => { this.tabHandler(1) }}>
                                             <span className="icon is-small"><i className="fa fa-paper-plane"></i></span>
                                             <span>Send Tokens</span>
                                         </a>
                                     </li>
-                                    <li>
-                                        <a>
+                                    <li className={this.state.classes[2]}>
+                                        <a onClick={() => { this.tabHandler(2) }}>
                                             <span className="icon is-small"><i className="fa fa-shopping-cart"></i></span>
                                             <span>Buy Tokens</span>
                                         </a>
                                     </li>
                                 </ul>
                             </div>
+                            <WalletInfo />
                             <Active tab={this.state.active} />
                         </div>
                     </div>
